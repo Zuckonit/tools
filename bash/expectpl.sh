757be745204e38d904a17bc7314f71c4ecbb393a
Zuckonit <lsin30@foxmail.com>
2013-08-26 07:47:26 +0800

    add file desc to expectpl.sh

--- bash/expectpl.sh
+++ bash/expectpl.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env expect
+#=============================================================================
+#     FileName: expectpl.sh
+#         Desc: this is a expect script tmplate
+#               install expect at first!!!
+#       Author: Mocker
+#        Email: Zuckerwooo@gmail.com
+#     HomePage: zuckonit.github.com
+#      Version: 0.0.1
+#   LastChange: 2013-08-26 07:44:50
+#      History:
+#=============================================================================
 
 set script [lindex $argv 0]
 set password [lindex $argv 1]
